4fa136f3afd88587573cf4b0d5fac5b226a54bc0
VictorDPA <vicnoob@gmail.com>
2023-11-02 16:44:14 -0300

    feat: initial product list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <main className="">
+    <div className="max-w-7xl mx-auto border pt-8 px-8 xl:px-0">
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:gap-5">
+        <div className="bg-white rounded-sm shadow-sm overflow-hidden">Prod</div>
+        <div className="bg-white rounded-sm shadow-sm overflow-hidden">Prod</div>
+        <div className="bg-white rounded-sm shadow-sm overflow-hidden">Prod</div>
+        <div className="bg-white rounded-sm shadow-sm overflow-hidden">Prod</div>
+      </section>
       <h1>Single Commerce</h1>
-    </main>
+    </div>
   )
 }
