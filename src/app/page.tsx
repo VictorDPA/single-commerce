e91f902115dbb5694f8fd3dd572f8303f097d2f3
VictorDPA <vicnoob@gmail.com>
2023-11-02 16:59:38 -0300

    feat: products types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
-export default function Home() {
-  return (
+import { ProductType } from "@/types/ProductType";
+async function getProducts() {
+  const res = await fetch('https://fakestoreapi.com/products')
+  if (!res.ok) {
+    throw new Error('Something went wrong')
+  }
+  return res.json()
+}
+
+export default async function Home() {
+  const products = await getProducts();
+    return (
     <div className="max-w-7xl mx-auto border pt-8 px-8 xl:px-0">
       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:gap-5">
-        <div className="bg-white rounded-sm shadow-sm overflow-hidden">Prod</div>
-        <div className="bg-white rounded-sm shadow-sm overflow-hidden">Prod</div>
-        <div className="bg-white rounded-sm shadow-sm overflow-hidden">Prod</div>
-        <div className="bg-white rounded-sm shadow-sm overflow-hidden">Prod</div>
+        {products.map((product: ProductType) => (
+          <div key={product.id} className="bg-white rounded-lg shadow divide-y divide-gray-200">
+            {product.title}
+            </div>
+            ))}
       </section>
-      <h1>Single Commerce</h1>
     </div>
   )
 }
