47c359e339ae352cd21eb8a0ccffdd63ccf5eeac
VictorDPA <vicnoob@gmail.com>
2023-11-02 18:19:23 -0300

    feat(provider): added clerk provider feat(localization): for clerk provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,31 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import clsx from 'clsx';
-import './globals.css';
-import Navbar from './components/Navbar';
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ptBR } from "@clerk/localizations";
+import { Inter } from "next/font/google";
+import clsx from "clsx";
+import "./globals.css";
+import Navbar from "./components/Navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Single E-Commerce',
-  description: 'Single E-Commerce - A Next.js starter for e-commerce sites',
-}
+  title: "Single E-Commerce",
+  description: "Single E-Commerce - A Next.js starter for e-commerce sites",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={clsx(inter.className, 'bg-slate-700')}>
-        <Navbar />
-        <main className='bg-slate-700 h-screen p-16'>
-        {children}
-        </main>
+    <ClerkProvider localization={ptBR}>
+      <html lang="en">
+        <body className={clsx(inter.className, "bg-slate-700")}>
+          <Navbar />
+          <main className="bg-slate-700 h-screen p-16">{children}</main>
         </body>
-    </html>
-  )
+      </html>
+    </ClerkProvider>
+  );
 }
