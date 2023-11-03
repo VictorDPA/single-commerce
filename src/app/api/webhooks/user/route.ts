2198fe96fae96acfd9ea6c3885cd950381990462
VictorDPA <vicnoob@gmail.com>
2023-11-03 18:16:50 -0300

    Feat(route): webhook between clerk and vercel

--- src/app/api/webhooks/user/route.ts
+++ src/app/api/webhooks/user/route.ts
@@ -4,14 +4,14 @@ import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook, WebhookRequiredHeaders } from "svix";
 
-const webHookSecret = process.env.CLERK_WEBHOOK_SECRET || '';
+const webHookSecret = process.env.CLERK_WEBHOOK_SECRET || "";
 
 type EventType = "user.created" | "user.updated" | "*";
 
 type Event = {
   data: EventDataType;
   type: EventType;
-  object: 'event';
+  object: "event";
 };
 
 type EventDataType = {
@@ -29,41 +29,51 @@ type EmailAddressType = {
 };
 
 async function handleWebhook(request: Request) {
-    const payload = await request.json();
-    const headersList = headers();
-    const heads = {
-        'svix-id': headersList.get('svix-id'),
-        'svix-timestamp': headersList.get('svix-timestamp'),
-        'svix-signature': headersList.get('svix-signature'),
-    }
+  const payload = await request.json();
+  const headersList = headers();
+  const heads = {
+    "svix-id": headersList.get("svix-id"),
+    "svix-timestamp": headersList.get("svix-timestamp"),
+    "svix-signature": headersList.get("svix-signature"),
+  };
 
-    const wh = new Webhook(webHookSecret);
-    let event: Event | null = null;
+  const wh = new Webhook(webHookSecret);
+  let event: Event | null = null;
 
-    try {
-      event = wh.verify(JSON.stringify(payload), heads as IncomingHttpHeaders & WebhookRequiredHeaders) as Event; 
-    } catch (e) {
-      console.error((e as Error).message);
-      return NextResponse.json({}, { status: 400 });
-    }
+  try {
+    event = wh.verify(
+      JSON.stringify(payload),
+      heads as IncomingHttpHeaders & WebhookRequiredHeaders,
+    ) as Event;
+  } catch (e) {
+    console.error((e as Error).message);
+    return NextResponse.json({}, { status: 400 });
+  }
 
-    const eventType: EventType = event.type;
-    if (eventType === "user.created" || eventType === "user.updated") {
-      const { id, first_name, last_name, email_addresses, primary_email_address_id, ...attributes } = event.data;
+  const eventType: EventType = event.type;
+  if (eventType === "user.created" || eventType === "user.updated") {
+    const {
+      id,
+      first_name,
+      last_name,
+      email_addresses,
+      primary_email_address_id,
+      ...attributes
+    } = event.data;
 
-      await prisma.user.upsert({
-        where: {externalId: id as string},
-        create: {
-          externalId: id as string,
-          attributes,
-        },
-        update: {
-          attributes,
-        }
-      });
-    }
+    await prisma.user.upsert({
+      where: { externalId: id as string },
+      create: {
+        externalId: id as string,
+        attributes,
+      },
+      update: {
+        attributes,
+      },
+    });
+  }
 
-    return NextResponse.json({}, { status: 200 });
+  return NextResponse.json({}, { status: 200 });
 }
 
 export const GET = handleWebhook;
