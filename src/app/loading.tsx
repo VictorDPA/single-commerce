ee5154926af4945fffed08dcb6637975d46b3cc5
VictorDPA <vicnoob@gmail.com>
2023-11-03 20:08:53 -0300

    Feat(SkeletonCard: created skeleton card for loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,15 @@
+import SkeletonCard from "./components/Skeleton";
+
 export default function Loading() {
-  return <h1>Loading</h1>;
+  return (
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:gap-5">
+        {Array.from({ length: 8 }).map((_, index) => (
+          <div key={index}>
+            <SkeletonCard isLoading />
+          </div>
+        ))}
+      </section>
+    </div>
+  );
 }
