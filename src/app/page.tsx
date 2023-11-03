9ce782bec59cb288d029d5dc99eaea57d9346d5e
VictorDPA <vicnoob@gmail.com>
2023-11-03 20:08:13 -0300

    Chorus: removed border at ln 33

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ async function getProducts(): Promise<ProductType[]> {
 export default async function Home() {
   const products = await getProducts();
   return (
-    <div className="max-w-7xl mx-auto border pt-8 px-8 xl:px-0">
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:gap-5">
         {products.map((product) => (
           <div key={product.id}>
