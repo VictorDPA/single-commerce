1db8a39c63159a6f4c8087900443ab1ebc68523c
VictorDPA <vicnoob@gmail.com>
2023-11-03 18:17:36 -0300

    Chorus: refactoring for new API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,32 @@
 import { ProductType } from "@/types/ProductType";
 import Product from "./components/Product";
+import Stripe from "stripe";
 
-async function getProducts() {
-  const res = await fetch('https://fakestoreapi.com/products')
-  if (!res.ok) {
-    throw new Error('Something went wrong')
-  }
-  return res.json()
+async function getProducts(): Promise<ProductType[]> {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: "2023-10-16",
+  });
+
+  const products = await stripe.products.list();
+
+  // const res = await fetch("https://fakestoreapi.com/products");
+  // if (!res.ok) {
+  //   throw new Error("Something went wrong");
+  // }
+  // return res.json();
 }
 
 export default async function Home() {
   const products = await getProducts();
-    return (
+  return (
     <div className="max-w-7xl mx-auto border pt-8 px-8 xl:px-0">
       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:gap-5">
         {products.map((product: ProductType) => (
           <div key={product.id}>
             <Product product={product} />
-            </div>
-            ))}
+          </div>
+        ))}
       </section>
     </div>
-  )
+  );
 }
