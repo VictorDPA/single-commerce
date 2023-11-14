7d3c19304808319c85805f8a5211cbd041096d35
VictorDPA <vicnoob@gmail.com>
2023-11-13 22:08:53 -0300

    Chore(price): editted functions for value

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,12 @@
-export const formatPrice = (price: number | null) => {
+export const formatPrice = (
+  price: number | null,
+  quantity: number | undefined | 1 = 1,
+) => {
   if (!price) return "R$ 0,00";
+  if (quantity === undefined) quantity = 1;
 
   return new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
-  }).format(price / 100);
-}
+  }).format((price * quantity) / 100);
+};
