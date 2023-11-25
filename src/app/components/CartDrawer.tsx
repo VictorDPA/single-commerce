4b7f138254f0daad824f691153be908340d4d706
VictorDPA <vicnoob@gmail.com>
2023-11-25 16:17:08 -0300

    Chorus(Checkout): Now with checkout button in drawer

--- src/app/components/CartDrawer.tsx
+++ src/app/components/CartDrawer.tsx
@@ -2,6 +2,8 @@
 import { formatPrice } from "@/lib/utils";
 import { useCartStore } from "@/store";
 import Image from "next/image";
+import CheckoutButton from "./CheckoutBtn";
+import Checkout from "./Checkout";
 
 export default function CartDrawer() {
   const useStore = useCartStore();
@@ -22,40 +24,52 @@ export default function CartDrawer() {
           Back to Store
         </button>
         <div className="border-t border-gray-400 my-4" />
-        {useStore.cart.map((item) => (
-          <div className="flex flex-col md:flex-row gap-4 py-3" key={item.id}>
-            {item.image && (
-              <Image
-                src={item.image}
-                alt={item.name}
-                width={64}
-                height={64}
-                className="w-20 h-20 object-cover"
-              />
-            )}
-            <div>
-              <h2 className="w-42 truncate">{item.name}</h2>
-              <h2>Quantidade: {item.quantity}</h2>
-              <p className="text-teal-600 font-bold text-sm">
-                {formatPrice(item.price, item.quantity)}
-              </p>
-              <div className="flex gap-4">
-                <button
-                  className="py-1 px-2 border rounded-md mt-2 text-sm mr-1"
-                  onClick={() => useStore.addToCart(item)}
-                >
-                  Adicionar
-                </button>
-                <button
-                  className="py-1 px-2 border rounded-md mt-2 text-sm mr-1"
-                  onClick={() => useStore.removeFromCart(item)}
-                >
-                  Remover
-                </button>
+        {useStore.onCheckout === "cart" && (
+          <>
+            {useStore.cart.map((item) => (
+              <div
+                className="flex flex-col md:flex-row gap-4 py-3"
+                key={item.id}
+              >
+                {item.image && (
+                  <Image
+                    src={item.image}
+                    alt={item.name}
+                    width={64}
+                    height={64}
+                    className="w-20 h-20 object-cover"
+                  />
+                )}
+                <div>
+                  <h2 className="w-42 truncate">{item.name}</h2>
+                  <h2>Quantidade: {item.quantity}</h2>
+                  <p className="text-teal-600 font-bold text-sm">
+                    {formatPrice(item.price, item.quantity)}
+                  </p>
+                  <div className="flex gap-4">
+                    <button
+                      className="py-1 px-2 border rounded-md mt-2 text-sm mr-1"
+                      onClick={() => useStore.addToCart(item)}
+                    >
+                      Adicionar
+                    </button>
+                    <button
+                      className="py-1 px-2 border rounded-md mt-2 text-sm mr-1"
+                      onClick={() => useStore.removeFromCart(item)}
+                    >
+                      Remover
+                    </button>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        ))}
+            ))}
+          </>
+        )}
+        {useStore.cart.length > 0 && useStore.onCheckout === "cart" && (
+          <CheckoutButton />
+        )}
+
+        {useStore.onCheckout === "checkout" && <Checkout />}
       </div>
     </div>
   );
